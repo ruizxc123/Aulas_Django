42c4047d38e9bc28922ce05b779a6a179523e3f8
ruizxc123 <167259567+ruizxc123@users.noreply.github.com>
2025-10-29 17:42:12 -0300

    Versão 5

--- tarefas/models.py
+++ tarefas/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Tarefa(models.Model):
+    titulo = models.CharField(max_length=255)
+    descricao = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
