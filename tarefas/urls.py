404a85fad05866048643464a0361a19a69a0abca
ruizxc123 <167259567+ruizxc123@users.noreply.github.com>
2025-10-30 18:09:58 -0300

    Versão 6

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.inicio, name='inicio')
+    path('', views.inicio, name='inicio'),
+    path('tarefa/<int:id>', views.TarefaView, name='tarefa-view' ),
+    path('novaTarefa/', views.novaTarefa, name='nova-tarefa' )
 ]
 
--- tarefas/views.py
+++ tarefas/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from . models import Tarefa
+from .forms import TarefaForm
 
 # Create your views here.
+
+
 def inicio(request):
-    return render(request, 'tarefas/index.html')
+    tarefas_list = Tarefa.objects.all().order_by('-created_at')
+    return render(request, 'tarefas/index.html', {'var_tarefas': tarefas_list})
+
+
+def TarefaView(request, id):
+    tarefa = get_object_or_404(Tarefa, pk=id)
+    return render(request, 'tarefas/view_tarefa.html', {'tarefa': tarefa})
+
+
+def novaTarefa(resquest):
+    if resquest.method == 'POST':
+        form = TarefaForm(resquest.POST)
+        form.save()
+        return redirect('/')
+
+    else:
+        form = TarefaForm()
+        return render(resquest, 'tarefa/addTarefa.html', {'form': form})
